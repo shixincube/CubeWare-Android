40d600f19fe0542c8cd7bec1ec7f5894edb86056
alex <120146859@qq.com>
2019-05-29 18:35:24 +0800

    update bat

--- CubeBuilder.bat
+++ CubeBuilder.bat
@@ -98,4 +98,20 @@ if exist cube-whiteboard (
 	) else (
 	echo cube-whiteboard missing
 	)
-
+set /p str=Whether to copy files to app/libs(y/n)?:
+if %str% == y (
+    copy .\cube-api\build\libs\cube-api*.jar .\app\libs
+    copy .\cube-call\build\libs\cube-call*.jar .\app\libs
+    copy .\cube-conference\build\libs\cube-conference*.jar .\app\libs
+    copy .\cube-core\build\libs\cube-core*.jar .\app\libs
+    copy .\cube-file\build\libs\cube-file*.jar .\app\libs
+    copy .\cube-instruction\build\libs\cube-instruction*.jar .\app\libs
+    copy .\cube-live\build\libs\cube-live*.jar .\app\libs
+    copy .\cube-media\build\libs\cube-media*.jar .\app\libs
+    copy .\cube-message\build\libs\cube-message*.jar .\app\libs
+    copy .\cube-sharedesktop\build\libs\cube-sharedesktop*.jar .\app\libs
+    copy .\cube-whiteboard\build\libs\cube-whiteboard*.jar .\app\libs
+    echo copy success
+) else (
+    echo Don't copy
+)
